8f018dda2b8295c1bc0c37b87695a691c2c987c6
pavlovka <stricker-fer@yandex.ru>
2022-04-01 00:30:05 +0300

    remove console.log

--- src/services/microsoft-app-center.service.js
+++ src/services/microsoft-app-center.service.js
@@ -5,7 +5,6 @@ const URL_API = apiBaseURL();
 const API_TOKEN = apiToken();
 
 export function getBranches(ownerName, applicationName) {
-    console.log(URL_API);
     const url = `${URL_API}/v0.1/apps/${ownerName}/${applicationName}/branches`;
 
     return axios.get(url, {
